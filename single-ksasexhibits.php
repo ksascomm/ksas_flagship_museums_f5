149b406adf4277c536ef64872cf02b3900269576
ksascomm <ksasweb@jhu.edu>
2015-03-19 13:01:19 -0400

    Updated Query & orbit data-options
    Rewrote query code so to run the conditional that hides the slider if
    no images attached. Updated old data-options code, removing navigation
    arrows, decreasing advance time, and increasing animation speed

--- single-ksasexhibits.php
+++ single-ksasexhibits.php
@@ -25,33 +25,35 @@
 
 			<div class="small-12 columns">
 				<div id="featured">
-					<ul id="slider" data-orbit data-options="animation: fade; animationSpeed:1000; timer:true; advanceSpeed:8000; navigation_arrows:true; bullets:false; slide_number:false;" class="exhibit-slide">
-					<?php $args = array( 
-						'post_type' => 'attachment', 
-						'orderby' => 'menu_order', 
-						'order' => 'ASC', 
-						'post_mime_type' => 'image',
-						'post_status' => null, 
-						'numberposts' => null, 
-						'post_parent' => $post->ID );
+						<?php $args = array( 
+					        'post_type' => 'attachment', 
+					        'orderby' => 'menu_order', 
+					        'order' => 'ASC', 
+					        'post_mime_type' => 'image',
+					        'post_status' => null, 
+					        'numberposts' => null, 
+					        'post_parent' => $post->ID 
+					    );
 
-						$attachments = get_posts($args);
-						if ($attachments) {
-							foreach ( $attachments as $attachment ) {
-					 			 $alt = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
-								 $image_title = $attachment->post_title;
-								 $caption = $attachment->post_excerpt;
-								 $description = $attachment->post_content;
-					?>	
-					
-					<li>
-					<div class="exhibit">
-					<img src="<?php echo wp_get_attachment_url($attachment->ID); ?>" alt="<?php echo $alt; ?>">
-					</div>
-					</li>
-					
- 					<?php } } ?>
-					</ul>
+					    $attachments = get_posts( $args );
+					    if( ! empty( $attachments ) ) :
+					?>
+					<ul id="slider" data-orbit data-options="animation: fade; animation_speed:2000; timer:true; advance_speed:1500; navigation_arrows:false; bullets:false; slide_number:false;" class="exhibit-slide">
+					  <?php foreach ( $attachments as $attachment ) {
+				            $alt = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
+				            $image_title = $attachment->post_title;
+				            $caption = $attachment->post_excerpt;
+				            $description = $attachment->post_content;
+			        	?>  
+							 <li>
+				                <div class="exhibit">
+				                    <img src="<?php echo wp_get_attachment_url($attachment->ID); ?>" alt="<?php echo $alt; ?>">
+				                </div>
+				            </li>
+
+				        <?php } ?>
+				    </ul>
+				<?php endif; ?>
 				</div>	
 			</div>
 		<div class="small-12 columns">
